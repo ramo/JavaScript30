253b96bff28db16259da55e8b3951a5edc9d9a64
Ramo <ramo.phoenix7@gmail.com>
2022-02-11 11:29:15 +0530

    Completed Unreal webcam fun project.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,89 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error("Camera permission is not there!!", err);
+        });
+}
+
+function paintToCanvas() {
+    const { videoWidth: width, videoHeight: height } = video;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        
+        // pixels = redEffect(pixels);
+        
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+        
+        pixels = greenScreen(pixels);
+
+        ctx.putImageData(pixels, 0, 0);    
+    }, 16);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i] += 100;
+        pixels.data[i + 1] -= 50;
+        pixels.data[i + 2] *= 0.5;
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+    document
+        .querySelectorAll('.rgb input')
+        .forEach(input => levels[input.name] = input.value);
+    
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        const [red, green, blue] = pixels.data.slice(i, i+3);
+        if  (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+                pixels.data[i + 3] = 0;
+            }
+    }
+    return pixels;
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src=${data} alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
